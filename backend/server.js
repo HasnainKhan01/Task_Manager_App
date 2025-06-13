f0293b0c5f6cfe8806b8c71cccaa1ab687ddd1f4
Shadab <skhan36@horizon.csueastbay.edu>
2025-06-12 20:30:24 -0700

    updated server.js to connect to mongoDB atlas with mongoose.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config();
 
@@ -9,6 +10,13 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
     res.send('Task Manager API is running');
